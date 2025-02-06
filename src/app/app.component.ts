9356d01115eedbfaa5da11c29be2276cdb96fc18
Artur <artur.rozwalak@gmail.com>
2025-02-06 14:07:14 +0000

    updated Angular to 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './domains/shared/ui-common/sidebar/sidebar.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, SidebarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    imports: [CommonModule, RouterOutlet, SidebarComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-portal';
