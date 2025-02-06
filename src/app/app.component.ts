4d2c343acafeca0c6d3763c63ef9f29c7fe4adc1
Artur <artur.rozwalak@gmail.com>
2025-02-06 14:49:38 +0000

    added TopbarComponent to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './domains/shared/ui-common/sidebar/sidebar.component';
+import { TopbarComponent } from './domains/shared/ui-common/topbar/topbar.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [CommonModule, RouterOutlet, SidebarComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [CommonModule, RouterOutlet, SidebarComponent, TopbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-portal';
