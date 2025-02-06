2261983569e2f5d1d89e298e679e0206405c138e
Artur <artur.rozwalak@gmail.com>
2025-02-06 15:32:14 +0000

    added HomeComponent, InboxComponent and set routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './domains/home/home.component';
+import { InboxComponent } from './domains/inbox/inbox.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'inbox',
+    component: InboxComponent,
+  },
+];
--- src/app/domains/shared/ui-common/sidebar/sidebar.component.ts
+++ src/app/domains/shared/ui-common/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
   faMagnifyingGlass,
@@ -8,7 +9,7 @@ import {
 
 @Component({
   selector: 'app-sidebar',
-  imports: [FontAwesomeModule],
+  imports: [RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
