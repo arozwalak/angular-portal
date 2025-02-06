1b0082e46f9ebb3d1cdcb41c8c4b8df0523e852f
Artur <artur.rozwalak@gmail.com>
2025-02-06 14:49:11 +0000

    added buttons to SidebarComponent

--- src/app/domains/shared/ui-common/sidebar/sidebar.component.ts
+++ src/app/domains/shared/ui-common/sidebar/sidebar.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faMagnifyingGlass,
+  faInbox,
+  faList,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-    selector: 'app-sidebar',
-    imports: [],
-    templateUrl: './sidebar.component.html',
-    styleUrl: './sidebar.component.scss'
+  selector: 'app-sidebar',
+  imports: [FontAwesomeModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+  faMagnifyingGlass = faMagnifyingGlass;
+  faInbox = faInbox;
+  faList = faList;
 }
