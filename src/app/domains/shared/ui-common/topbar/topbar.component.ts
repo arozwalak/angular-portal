71c15234b526a30088c13e07f49b717bec2f6b97
Artur <artur.rozwalak@gmail.com>
2025-02-07 14:53:32 +0000

    added currentUserFullName to topBar

--- src/app/domains/shared/ui-common/topbar/topbar.component.ts
+++ src/app/domains/shared/ui-common/topbar/topbar.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import {Router, RouterLink} from "@angular/router";
+import {AuthService} from "@app/shared/util-auth/auth.service";
+import {map, take} from "rxjs";
 
 @Component({
   selector: 'app-topbar',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss',
 })
 export class TopbarComponent {
+  readonly #authService = inject(AuthService);
+  readonly #router = inject(Router);
   faUser = faUser;
+  readonly currentUserFullName = this.#authService.currentUserFullName;
+
+  logout() {
+    this.#authService.logout().pipe(
+      take(1),
+      map((response) => {
+        if (response === 'success') {
+          this.#router.navigate(['/login']);
+        }
+      })
+    ).subscribe();
+  }
 }
